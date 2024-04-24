8506101b3327f9a6e0b5ea6d03dcdff31c64e46c
MACHG20 <167126922+MACHG20@users.noreply.github.com>
2024-04-23 22:58:57 -0500

    Update ShoppingCart.cpp
    updated many of the getters, setters, and constructors. Still have to complete print functions.

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -3,28 +3,72 @@ using namespace std;
 
 #include "ShoppingCart.h"
 
-public:
+//constructors
+ShoppingCart::ShoppingCart();
+ShoppingCart::ShoppingCart(string name, string date){
+   customerName = name;
+   currentDate = date;
+}
+
+
+//getters
 string ShoppingCart::GetCustomerName(){
    return name;
    }
 string ShoppingCart::GetDate(){
    return date;
    }
-void ShoppingCart::AddItem(){
-   }
-void ShoppingCart::RemoveItem(){
-   }
-void ShoppingCart::ModifyItem(){
-   }
 int ShoppingCart::GetNumItemsInCart(){
-   return numItemsInCart();
+   return cartItems.size();
    }
 double ShoppingCart::GetCostOfCart(){
+   double costOfCart = 0.0;
+    for(int i = 0; i<cartItems.size(); ++i){
+       costOfCart += cartItems.at(i).GetPrice()*cartItems.at(i).GetQuantity();
+    }
    return costOfCart
    }
+
+//Mutators
+void ShoppingCart::AddItem(ItemToPurchase item){
+   cartItems.push_back(item);
+   }
+
+void ShoppingCart::RemoveItem(string itemName){
+   bool Found = false;
+   for(int i = 0; i<cartItems.size(); ++i){
+      if(cartItems.at(i).GetName() == itemName){
+         
+         //still need to remove item
+         
+         Found = true;
+         break;
+      }
+   }
+   if(!Found){
+      cout << "Item not found in cart. Nothing removed." <<endl;
+   }
+}
+
+void ShoppingCart::ModifyItem(ItemToPurchase item){
+   bool found = false;
+   for(int i = 0; i<cartItems.size(); ++i){
+      if(cartItems.at(i).GetName() == item.GetName()){
+
+         //have to check if parameter has default values
+         //modify if it does not         
+      }
+      found = true;
+      break;
+   }
+   if(!Found){
+      cout << "Item not found in cart. Nothing removed." <<endl;
+   }
+}
+
+//Print Functions
 void ShoppingCart::PrintTotal(){
    cout << ItemToPurchase.costOfCart << endl;
-   
    }
 void ShoppingCart::PrintDescriptions(){
    cout << ItemToPurchase.description << endl;
