9b411ff9827b7828c58b6247dbbe69b66be8cb25
JonathanHavard <167351822+JonathanHavard@users.noreply.github.com>
2024-04-28 15:36:44 -0500

    Update ItemToPurchase.cpp
    It now Compiles

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,49 @@
-/* Type your code here */
+#include <iostream>
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+//define constructor
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
+  itemName = "";
+  itemDescription = "";
+  itemPrice = 0;
+  itemQuantity = 0;
+}
+
+//define mutators
+void ItemToPurchase::SetName(string name){
+  itemName = name;
+}
+void ItemToPurchase::SetDescription(string description){
+  itemDescription = description;
+}
+void ItemToPurchase::SetPrice(int price){
+  itemPrice = price;
+}
+void ItemToPurchase::SetQuantity(int quantity){
+  itemQuantity = quantity;
+}
+
+//define getters
+string ItemToPurchase::GetName() const{
+  return itemName;
+}
+string ItemToPurchase::GetDescription() const{
+  return itemDescription;
+}
+int ItemToPurchase::GetPrice() const{
+  return itemPrice;
+}
+int ItemToPurchase::GetQuantity() const{
+  return itemQuantity;
+}
+
+//define print functions
+void ItemToPurchase::PrintItemCost(){
+  cout << GetName() << " " << GetQuantity() << " @ $" << GetPrice() << " = $" << itemQuantity * itemPrice << endl;
+}
+void ItemToPurchase::PrintItemDescription(){
+  cout << GetName() << ": " << GetDescription() << endl;
+}
+   
